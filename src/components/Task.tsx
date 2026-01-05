ac1c7f83905274485c110fee089e755195c86c27
fwttnnn <lolywk@tutanota.com>
2026-01-06 00:54:33 +0700

    feat: react router

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,14 +3,37 @@ import type { Label, Task } from "@/hooks/useTasks"
 
 export default () => {
   /**
-   * 31.1 (hooks)
+   * 31.1 (custom hooks)
+   * 12.2 (conditional rendering)
    */
   const tasks = useTasks()
   if (!tasks) return <></>
 
   return (
+    <>
+      <Toolbar />
+      <List tasks={tasks} />
+    </>
+  )
+}
+
+export const Toolbar = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+/**
+ * 9.2 (props)
+ */ 
+export const List = ({ tasks }: { tasks: Array<Task> }) => {
+  return (
     <ul>
       {tasks.map((task: Task) => (
+        /**
+         * 26.4 (tailwind)
+         */
         <li
           className="py-1.5 my-2"
         >
@@ -34,6 +57,9 @@ export default () => {
                   <div key={`--label-${label.id}`}>
                     <span
                       title={label.name}
+                      /**
+                       * 12.5 (pure css)
+                       */
                       style={{ background: label.theme.bg,
                                color: label.theme.fg, }}
                       className="rounded-full px-2 py-1"
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,44 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+/**
+ * 12.5 (pure css)
+ */
 import "@/main.css"
 
 import Layout from "@/components/layouts"
 import Task from "@/components/Task"
 
 /**
-  * 17.4 (HOC) from React itself.
-  */
+ * 17.4 (HOC) from React itself.
+ */
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Layout>
-      <Task />
-    </Layout>
+    {/**
+      * 12.4 (React Router)
+      */}
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={"/"}
+          element={
+            <Layout>
+              <Task />
+            </Layout>
+          }
+        />
+        <Route
+          path={"*"}
+          element={
+            <Layout>
+              <p>
+                Oops! the page you are looking for is not found. <a href="/">Go back!</a>
+              </p>
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
