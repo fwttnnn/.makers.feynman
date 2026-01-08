3d1894aa07675cf3747d29e1dc2ea88b319fe35e
fwttnnn <lolywk@tutanota.com>
2026-01-08 12:57:50 +0700

    chore: remove unused import

--- src/components/Settings.test.tsx
+++ src/components/Settings.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react"
-import { describe, it, expect, vi } from "vitest"
+import { describe, it, expect } from "vitest"
 
 import Settings from "@/components/Settings"
 import styles from "@/stores/redux/style/constants"
