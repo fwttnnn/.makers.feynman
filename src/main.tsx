6555ff45e34cf84e276a6405c5adcc2ea6a90655
fwttnnn <lolywk@tutanota.com>
2026-01-08 13:30:28 +0700

    fix: re-introduce lazy()

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react"
+import { StrictMode, lazy } from "react"
 import { createRoot } from "react-dom/client"
 import { HelmetProvider, Helmet } from "react-helmet-async"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -26,13 +26,9 @@ import Boundary from "@/components/Boundary"
 /**
  * 17.5 (performance opt.)
  */
-// const Layout   = lazy(() => import("@/components/Layouts"))
-// const Task     = lazy(() => import("@/components/Task"))
-// const Settings = lazy(() => import("@/components/Settings"))
-
-import Layout from "@/components/Layouts"
-import Task from "@/components/Task"
-import Settings from "@/components/Settings"
+const Layout   = lazy(() => import("@/components/Layouts"))
+const Task     = lazy(() => import("@/components/Task"))
+const Settings = lazy(() => import("@/components/Settings"))
 
 /**
  * 17.4 (HOC) from React itself.
